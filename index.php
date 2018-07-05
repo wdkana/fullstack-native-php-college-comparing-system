e2150209e6042773f403c2a77fd6f7f6eeadec35
afrizaldea <afrizaldea@gmail.com>
2018-07-06 00:21:22 +0700

    remove semuakampus tab

--- index.php
+++ index.php
@@ -62,7 +62,6 @@
 		<div class="ui top attached tabular menu animated fadeInDownBig">
 		  	<a class="item active" data-tab="bandingkan">Bandingkan</a>
 		  	<a class="item" data-tab="peringkat">Peringkat</a>
-		  	<a class="item" data-tab="semuakampus">Semua Kampus</a>
 		  	<a class="item" data-tab="masuk">Masuk</a>
 		</div>
 
@@ -168,7 +167,7 @@
     					<tr>
     						<th colspan="4">
       							<div class="ui right floated pagination menu">
-        							
+        							<!-- untuk pagination -->
       							</div>
     						</th>
   						</tr>
@@ -177,17 +176,6 @@
 			</div>
 		</div>
 
-		<!-- tab3 -->
-		<div class="ui bottom attached tab segment" data-tab="semuakampus"">
-
-			<div class="ui hidden divider"></div>
-
-			<div class="ui justified aligned container">
-				<h1>SEMUA KAMPUS</h1>
-	  			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-			</div>
-
-		</div>
 
 		<!-- tab4 -->
 		<div class="ui bottom attached tab segment" data-tab="masuk"">
